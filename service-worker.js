7321ce9fc73a822ac7b129b77c927faecf75cd1d
Dwardell88 <davidw@sandboxdlabs.com>
2026-08-08 13:32:54 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker v1.20
-const CACHE_NAME = 'forge-radio-v45'; // Incremented to v36 to force Charles's phone to update
+const CACHE_NAME = 'forge-radio-v46'; // Incremented to v36 to force Charles's phone to update
 
 self.addEventListener('install', e => {
     self.skipWaiting();
