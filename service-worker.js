c5611982f69848b05857880427c249337ccb1bb1
Dwardell88 <davidw@sandboxdlabs.com>
2026-08-12 12:07:02 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "forge-pwa-v3-responsive-safe-nav";
+const CACHE = "forge-pwa-v5-responsive-safe-nav";
 const CORE = [
   "/", "/home/", "/news/", "/request/", "/shows/", "/our-partners/",
   "/prayer-wall/", "/install/", "/offline.html", "/manifest.webmanifest",
